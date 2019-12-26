45ceda1cc5d1c4330a67d819b7a628cf098c1a1d
ml <xxx@123.com>
2019-12-26 12:20:22 +0800

    自动登录完成

--- serv/users.py
+++ serv/users.py
@@ -15,6 +15,23 @@ def reg():
     res = MONGO_DB.users.insert_one(user_info)
     RET["code"] = 0
     RET["msg"] = "用户注册成功"
-    RET["data"] = {"user_id":str(res.inserted_id)}
+    RET["data"] = {"user_id": str(res.inserted_id)}
+
+    return jsonify(RET)
+
+
+@users.route('/login', methods=["post"])
+def login():
+    user = request.form.to_dict()
+    # print(user)
+    user = MONGO_DB.users.find_one(user, {"password": 0})
+    if user:
+        RET["code"] = 0
+        RET["msg"] = "登录成功"
+        user["_id"] = str(user.get("_id"))
+        RET["data"] = user
+    else:
+        RET["code"] = -1
+        RET["msg"] = "用户名密码错误"
 
     return jsonify(RET)
